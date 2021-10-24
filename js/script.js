fa05c7edcd65fa56dfe0b3f755e50ed2bc2c05e1
Seeker3000 <tecdava@tutanota.com>
2021-10-24 20:05:50 +0300

    IoT

--- js/script.js
+++ js/script.js
@@ -66,6 +66,9 @@ function NAV12() {
 function NAV13() {
   document.getElementById("vru-meniu-13").classList.toggle("show");
 }
+function NAV14() {
+  document.getElementById("vru-meniu-14").classList.toggle("show");
+}
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.submenu-vru')) {
